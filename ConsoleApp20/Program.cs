aafec2c6021049ab7dcbdeb0b4f366c2f19196e5
huseynManaf <huseynim@code.edu.az>
2022-12-22 06:41:58 -0800

    init

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salam Dostlar");
+            int saniye = 2788972;
+            int gun = saniye/(24 * 60 * 60);
+            int saat = (saniye - gun * 24 * 60 * 60) / (60 * 60);
+            int deq = (saniye - gun * 24 * 60 * 60 - saat * 60 * 60) / 60;
+            int san = saniye % 60;
+            Console.WriteLine($"{gun} gun {saat} saat {deq} deqiqe  { san} saniye ");
+            Console.WriteLine("master");
+        }
+    }
+}
